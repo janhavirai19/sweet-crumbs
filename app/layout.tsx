f976f4bcfa24d0b355d0c15567293b8e27d7cba4
janhavirai19 <janhavirai932@gmail.com>
2026-09-06 22:40:03 +0530

    Update Sweet Crumbs favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Sweet Crumbs|Cakes & Bakes",
+  title: "Sweet Crumbs | Cakes & Bakes",
   description: "Sweet Crumbs - Fresh Cakes & Bakes",
 };
 
